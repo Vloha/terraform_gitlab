820e7ba81e2b80e1da9b3509ebeba2f35960fa93
Vloha <49884365+Vloha@users.noreply.github.com>
2022-06-30 13:30:20 +0300

    Update Bastions.tf

--- Modules/Bastion_instances/Bastions.tf
+++ Modules/Bastion_instances/Bastions.tf
@@ -38,7 +38,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "RunnerBastion" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
-    key_name = "Vgitlab"
+    key_name = "*"
     vpc_security_group_ids = [aws_security_group.Bastionsg.id]
     subnet_id = var.public_subnet_id
     associate_public_ip_address = true
